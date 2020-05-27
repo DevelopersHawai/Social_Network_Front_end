79cc6fe0a4013ba78a8e29261e8e0e50cafd4a35
Corey Albright <coreymalbright@gmail.com>
2020-05-27 03:55:30 -0400

    menu bar style and tab spaces

---
+++ src/core/Menu.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+// The tabs and style come from boot strap material site
+//bootsrap material designß
+const Menu = () => (
+                              <div> 
+
+<ul className="nav nav-tabs">
+        <li className="nav-item">
+            <Link className="nav-link" to="/"> Home </Link>
+       </li>
+       <li className="nav-item">
+            <Link className="nav-link" to="/signup">  Sign-Up  </Link>
+        </li>
+       <li className="nav-item">
+            <Link className="nav-link" to="/signin"> Sign-In  </Link>
+            </li>
+            </ul>
+  </div>
+
+
+);
+
+
+export default Menu; 
