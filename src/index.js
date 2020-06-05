4973d2e267fc39ede081d6e75575966637e5ddee
Corey Albright <coreymalbright@gmail.com>
2020-06-05 02:16:39 -0400

    Added_Queue

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));  //takes render function from within the:
 
 //App.js file and places a div tag that says React FrontEnd
+
+
