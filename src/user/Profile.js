767a023c80ff58410294aaf3addb465731e50592
Corey Albright <coreymalbright@gmail.com>
2020-06-10 01:26:42 -0400

    added buttons edit profile and delete btns

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {isAuthenticated} from '../auth'
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import {read} from "./apiUser";
 
 // Make a request to the back-end to get the a user's information
@@ -39,17 +39,47 @@ componentDidMount() {
 
     render() {
 
-        const redirectToSignin= this.state.redirectToSignin
+        const {redirectToSignin, user}= this.state;
             if(redirectToSignin) return <Redirect to="/signin" />
         return (
+            //Create the container  object to hold into columns 
             <div className="container">
+                <div className="row">
+                <div className="col-md-6">
                 <h2 className="mt-5 mb-5">
                 Profile
-        <p>Hello {isAuthenticated().user.name} </p>
-        <p>Email:  {isAuthenticated().user.email} </p>
-        <p>{`Joined [Site name] ${new Date(this.state.user.created)
-            .toDateString()}`}</p>
                 </h2>
+        <p>Hello {isAuthenticated().user.name} </p>
+            <p>
+            Email:  {isAuthenticated().user.email} 
+            </p>
+            <p>
+            {`Joined [Site name] ${new Date
+        (user.created)
+            .toDateString()}`}
+            </p>    
+                </div>
+                {/* divide the container into columns left and right */}
+              <div className="col-md-6">
+                {isAuthenticated().user &&
+                 isAuthenticated().user._id === user._id &&
+                 (<div className="d-inline-block mt-5">
+                     <Link 
+                     className="btn btn-raised btn-sucess mr-5"
+                     to={`/user/edit/${user._id}`}>
+                     Edit Profile
+                     </Link>
+
+                    <button className="btn btn-raised btn-danger">
+                    Delete Profile
+
+
+                    </button>
+                         
+              </div>
+ )}
+                </div>
+            </div>
             </div>
         );
     }
