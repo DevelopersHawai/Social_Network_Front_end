54a8fb40eed6991ac68dab6e00230884c825521d
Corey Albright <coreymalbright@gmail.com>
2020-09-28 00:24:07 -0400

    adds the needed component and  fetches all users

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -1,4 +1,4 @@
-export const     read = (userId, token) => {
+export const read = (userId, token) => {
     //we get the user information once we make the request
     return fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
         method: "GET",
@@ -12,6 +12,16 @@ export const     read = (userId, token) => {
     
         }
     })
+    .then(response => {
+        return response.json();
+    })
+        .catch(err => console.log(err));
+        };
+
+        export const list = () => {
+    return fetch(`${process.env.REACT_APP_API_URL}/users`, {   
+        method: "GET",
+})
     .then(response => {
         return response.json();
     })
