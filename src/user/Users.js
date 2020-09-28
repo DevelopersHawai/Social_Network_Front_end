b3ebe0b5bdd652935801dd810ac351b2ae91e32f
Corey Albright <coreymalbright@gmail.com>
2020-09-28 17:37:57 -0400

    added some card style text

--- src/user/Users.js
+++ src/user/Users.js
@@ -18,12 +18,36 @@ class Users extends Component {
             
         });
     } 
+
+renderUsers = (users) => (
+    <div >
+    {users.map((user, i) => (
+       <div className="card" style={{width: '18rem'}} key={i}>
+       <img className="card-img-top" src="" alt="Card image cap" />
+       <div className="card-body">
+         <h5 className="card-title">Card title</h5>
+         <p className="card-text">Some quick example text to build on the card title
+                                  and make up the bulk of the card's content.
+                                  </p>
+         <a href="#" className="btn btn-primary">
+             Go somewhere
+             </a>
+       </div>
+     </div>
+    ))} 
+   
+ </div>
+);
+
+
     render() {
+        const {users} = this.state
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Users</h2>
-
+               {this.renderUsers(users)}
             </div>
+        
         );
     }
 }
