604b74b246cfc969ad09a2310ef561cf5b12adf1
Corey Albright <coreymalbright@gmail.com>
2020-09-28 17:52:05 -0400

    class 127 card styles

--- src/user/Users.js
+++ src/user/Users.js
@@ -20,17 +20,16 @@ class Users extends Component {
     } 
 
 renderUsers = (users) => (
-    <div >
+    <div className="row">
     {users.map((user, i) => (
-       <div className="card" style={{width: '18rem'}} key={i}>
+       <div className="card col-md-4"  key={i}>
        <img className="card-img-top" src="" alt="Card image cap" />
        <div className="card-body">
-         <h5 className="card-title">Card title</h5>
-         <p className="card-text">Some quick example text to build on the card title
-                                  and make up the bulk of the card's content.
+         <h5 className="card-title">{user.name}</h5>
+         <p className="card-text">{user.email}
                                   </p>
-         <a href="#" className="btn btn-primary">
-             Go somewhere
+         <a href="#" className="btn btn-raised btn-primary btn-sm">
+             View Profile
              </a>
        </div>
      </div>
