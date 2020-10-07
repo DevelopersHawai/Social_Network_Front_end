7ca8b976cc8231730fb5039045a0d0c54eae8f3d
Ervin <ervinjoel2323@gmail.com>
2020-10-06 22:59:24 -0600

    habilitando la opcion en el menu superior para devolverse al perfil personal.

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -38,7 +38,7 @@ componentDidMount() {
     this.init(userId);
 };
 
-componentWillReceiveProps(props) {
+componentWillReceiveProps(props) {   //habilitando la opcion en el menu superior para devolverse al perfil personal.
     const userId = props.match.params.userId;
     this.init(userId);
 }
